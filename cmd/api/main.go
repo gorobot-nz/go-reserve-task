47fecec0f97fe4d749e51a6cef4631bfbaf8c7cf
gorobot <gorobot.nz@gmail.com>
2021-12-20 14:31:25 +0300

    Create server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"go-tech-task/internal"
+	"log"
+)
+
+func main() {
+	server := new(internal.Server)
+	if err := server.Run("8000"); err != nil {
+		log.Fatalf("Server error: %s", err.Error())
+	}
+}
--- internal/server.go
+++ internal/server.go
@@ -1,1 +1,25 @@
 package internal
+
+import (
+	"context"
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	server *http.Server
+}
+
+func (s *Server) Run(port string) error {
+	s.server = &http.Server{
+		Addr:           ":" + port,
+		MaxHeaderBytes: 1 << 20,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+	}
+	return s.server.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.server.Shutdown(ctx)
+}
