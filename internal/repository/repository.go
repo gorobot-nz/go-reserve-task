4a78842f4a73bde0543fecc4ea95ec7063f3be69
gorobot <gorobot.nz@gmail.com>
2021-12-21 11:27:08 +0300

    Define repository

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,1 +1,15 @@
 package repository
+
+import "go-tech-task/internal/domain"
+
+type BookRepository interface {
+	GetBooks() ([]domain.Book, error)
+	GetBookById(id int64) (domain.Book, error)
+	AddBooks(book *domain.Book) int64
+	DeleteBook(id int64) (int64, error)
+	UpdateBook(id int64) (int64, error)
+}
+
+type Repository struct {
+	BookRepository
+}
