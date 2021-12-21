a990708642c8b727dfd0a08577f23e1b6e355be6
gorobot <gorobot.nz@gmail.com>
2021-12-21 11:39:33 +0300

    Define BookRepository

--- internal/repository/postgres_book.go
+++ internal/repository/postgres_book.go
@@ -1,1 +1,31 @@
 package repository
+
+import "go-tech-task/internal/domain"
+
+type LocalBook struct {
+	books []domain.Book
+}
+
+func NewLocalBook(books []domain.Book) *LocalBook {
+	return &LocalBook{books: books}
+}
+
+func (l LocalBook) GetBooks() ([]domain.Book, error) {
+	panic("implement me")
+}
+
+func (l LocalBook) GetBookById(id int64) (domain.Book, error) {
+	panic("implement me")
+}
+
+func (l LocalBook) AddBooks(book *domain.Book) int64 {
+	panic("implement me")
+}
+
+func (l LocalBook) DeleteBook(id int64) (int64, error) {
+	panic("implement me")
+}
+
+func (l LocalBook) UpdateBook(id int64) (int64, error) {
+	panic("implement me")
+}
