2c6cd23451607ea9914242a6c5f8648e16cc381b
gorobot <gorobot.nz@gmail.com>
2021-12-21 11:16:29 +0300

    Refactor

---
+++ internal/book/handler/http_book.go
@@ -0,0 +1,23 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func (h *Handler) GetBooks(context *gin.Context) {
+
+}
+
+func (h *Handler) GetBookById(context *gin.Context) {
+
+}
+
+func (h *Handler) AddBooks(context *gin.Context) {
+
+}
+
+func (h *Handler) DeleteBook(context *gin.Context) {
+
+}
+
+func (h *Handler) UpdateBook(context *gin.Context) {
+
+}
