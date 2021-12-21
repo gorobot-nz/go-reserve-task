a00e402416de13c15435e2178f1fd5a8cb4a6eb8
gorobot <gorobot.nz@gmail.com>
2021-12-21 18:41:33 +0300

    Add required field

--- internal/domain/book.go
+++ internal/domain/book.go
@@ -1,8 +1,8 @@
 package domain
 
 type Book struct {
-	ID      int64    `json:"id"`
-	Title   string   `json:"title"`
-	Authors []string `json:"authors"`
-	Year    string   `json:"year"`
+	ID      int64    `json:"id" binding:"required"`
+	Title   string   `json:"title" binding:"required"`
+	Authors []string `json:"authors" binding:"required"`
+	Year    string   `json:"year" binding:"required"`
 }
