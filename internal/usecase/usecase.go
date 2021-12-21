185a4857068940c502cc1e5c82321436269f314a
gorobot <gorobot.nz@gmail.com>
2021-12-21 11:26:49 +0300

    Define usecase

--- internal/usecase/usecase.go
+++ internal/usecase/usecase.go
@@ -1,1 +1,18 @@
 package usecase
+
+import (
+	"go-tech-task/internal/domain"
+)
+
+type BookUsecase interface {
+	GetBooks() ([]domain.Book, error)
+	GetBookById(id int64) (domain.Book, error)
+	AddBooks(book *domain.Book) int64
+	DeleteBook(id int64) (int64, error)
+	UpdateBook(id int64) (int64, error)
+}
+
+type Usecase struct {
+	BookUsecase
+}
+
