6f893737c28e6d8b90fdd7650f6a96d0bb16fe3f
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2021-12-24 09:04:17 +0300

    Add params to SQL

--- internal/schema/book.sql
+++ internal/schema/book.sql
@@ -1,6 +1,21 @@
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+    RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
 CREATE TABLE IF NOT EXISTS books(
     id serial PRIMARY KEY NOT NULL UNIQUE,
     title varchar(255) NOT NULL,
     authors varchar(255)[],
-    book_year date
-)
+    book_year date,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TRIGGER set_timestamp
+    BEFORE UPDATE ON books
+    FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
