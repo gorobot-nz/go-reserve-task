3de5c7b9b04a18923279477ac14fdf9b8f14f8f5
gorobot-nz <gorobot.nz@gmail.com>
2021-12-28 08:47:17 +0300

    Add TestBookUseCase_AddBooks and TestBookUseCase_DeleteBook

--- internal/book/usecase/usecase_test.go
+++ internal/book/usecase/usecase_test.go
@@ -1,1 +1,49 @@
 package usecase
+
+import (
+	"context"
+	"github.com/lib/pq"
+	"github.com/stretchr/testify/assert"
+	"go-tech-task/internal/book/repository/mock"
+	"go-tech-task/internal/domain"
+	"testing"
+)
+
+func TestBookUseCase_AddBooks(t *testing.T) {
+	rp := new(mock.BooksPostgresStorageMock)
+
+	uc := NewBookUseCase(rp)
+
+	testBook := domain.Book{
+		Authors: pq.StringArray{"First", "second"},
+		Year:    "2006-01-02",
+	}
+	ctx := context.Background()
+	rp.On("AddBooks", testBook).Return(testBook.ID, nil)
+	_, err := uc.AddBooks(ctx, testBook)
+	assert.NoError(t, err)
+}
+
+func TestBookUseCase_DeleteBook(t *testing.T) {
+	rp := new(mock.BooksPostgresStorageMock)
+
+	uc := NewBookUseCase(rp)
+
+	ctx := context.Background()
+	rp.On("DeleteBook", int64(1)).Return(int64(1), nil)
+	id, err := uc.DeleteBook(ctx, int64(1))
+	assert.NoError(t, err)
+	assert.Equal(t, int64(1), id)
+}
+
+func TestBookUseCase_GetBooks(t *testing.T) {
+
+}
+
+func TestBookUseCase_UpdateBook(t *testing.T) {
+
+}
+
+func TestBookUseCase_GetBookById(t *testing.T) {
+
+}
