29c85049c618472a95c5a7ecd337927d0b6b09a6
gorobot-nz <gorobot.nz@gmail.com>
2021-12-29 22:14:04 +0300

    Set up prometheus

--- pkg/middleware/prometheus.go
+++ pkg/middleware/prometheus.go
@@ -1,1 +1,13 @@
 package middleware
+
+import "github.com/prometheus/client_golang/prometheus"
+
+type PrometheusMiddleware struct {
+	serviceName     string
+	requestCount    *prometheus.CounterVec
+	requestDuration *prometheus.HistogramVec
+}
+
+func NewPrometheusMiddleware() *PrometheusMiddleware {
+	return &PrometheusMiddleware{}
+}
