fdcea9f9e1c2ae97e0b54c9f9f8171376e04a6b4
gorobot-nz <gorobot.nz@gmail.com>
2022-01-03 21:51:10 +0300

    Implement repository interface

--- internal/book/repository/elastic/book_elastic.go
+++ internal/book/repository/elastic/book_elastic.go
@@ -3,6 +3,10 @@ package elastic
 import (
 	"github.com/olivere/elastic"
 	"github.com/sirupsen/logrus"
+
+	"go-tech-task/internal/domain"
+
+	"context"
 )
 
 type BooksElasticStorage struct {
@@ -12,7 +16,32 @@ type BooksElasticStorage struct {
 func NewBooksElasticStorage() *BooksElasticStorage {
 	client, err := elastic.NewClient()
 	if err != nil {
-		logrus.Fatal("Error")
+		logrus.Fatalf("Error elastic client: %+v", err)
 	}
 	return &BooksElasticStorage{client}
+}
+
+func (b *BooksElasticStorage) GetBooks(ctx context.Context) ([]domain.Book, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (b *BooksElasticStorage) GetBookById(ctx context.Context, id int64) (*domain.Book, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (b *BooksElasticStorage) AddBooks(ctx context.Context, book domain.Book) (int64, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (b *BooksElasticStorage) DeleteBook(ctx context.Context, id int64) (int64, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (b *BooksElasticStorage) UpdateBook(ctx context.Context, id int64, book domain.Book) (int64, error) {
+	//TODO implement me
+	panic("implement me")
 }
